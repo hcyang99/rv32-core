7050da258d6d473f27040cc299a6441d4c62bdf3
sunsusan <sunsusan@umich.edu>
2020-04-12 13:56:54 -0400

    dcache.sv and dcache_ctrl.sv added

---
+++ verilog/dcache.sv
@@ -0,0 +1,357 @@
+// cachemem32x64
+// write-back no-write-allocate dcache
+
+`define BYTE 2'b0
+`define HALF 2'h1
+`define WORD 2'h2
+`define DOUBLE 2'h3
+`define MEM_SIZE [1:0]
+`define LSQSZ 16
+
+module dcache(
+    input                       clock,
+    input                       reset, 
+
+    input                       proc_wr_en,
+    input [2:0]                 proc_wr_offset,
+    input [4:0]                 proc_wr_idx,
+    input [7:0]                 proc_wr_tag,
+    input [63:0]                proc_wr_data, 
+    input `MEM_SIZE             proc_wr_size,
+
+    input                       mem_wr_en,
+    input [4:0]                 mem_wr_idx,
+    input [7:0]                 mem_wr_tag,
+    input [63:0]                mem_wr_data, 
+
+    input                       rd_en,
+    input [2:0]                 rd_offset,
+    input [4:0]                 rd_idx,
+    input [7:0]                 rd_tag,
+    input `MEM_SIZE             rd_size,
+    input [`LSQSZ-1:0]          rd_gnt,
+
+    output logic [63:0]         rd_data,
+    output logic                rd_valid,
+
+    // write dirty entries back
+    output logic                wb_en_out,
+    output logic [15:0]         wb_addr_out,
+    output logic [63:0]         wb_data_out,
+    output logic `MEM_SIZE      wb_size_out,
+
+    // write directly to mem on wr miss
+    output logic                wr_en_out,
+    output logic [15:0]         wr_addr_out,
+    output logic [63:0]         wr_data_out,
+    output logic `MEM_SIZE      wr_size_out,
+
+    // read from mem on rd miss
+    output logic                rd_en_out,
+    output logic [15:0]         rd_addr_out,
+    output logic `MEM_SIZE      rd_size_out,
+    output [`LSQSZ-1:0]         rd_gnt_out
+);
+
+reg [31:0] [63:0] data;
+reg [31:0] [7:0] tags; 
+reg [31:0] valid;
+reg [31:0] dirty;
+reg [1:0] [12:0] victim_tags;
+reg [1:0] [63:0] victim_data;
+reg [1:0] victim_valid;
+reg [1:0] victim_dirty;
+reg victim_lru;
+
+wire [63:0] rd_data_cache_raw;
+wire [63:0] rd_data_cache;
+wire [63:0] rd_data_victim;
+
+logic [63:0] proc_wr_mask;
+logic [63:0] proc_rd_mask;
+
+logic [1:0] [12:0] victim_tags_after_wr;
+logic [1:0] [63:0] victim_data_after_wr;
+logic [1:0] victim_valid_after_wr;
+logic [1:0] victim_dirty_after_wr;
+logic victim_lru_after_wr;
+logic [31:0] [63:0] data_after_wr;
+logic [31:0] [7:0] tags_after_wr; 
+logic [31:0] valid_after_wr;
+logic [31:0] dirty_after_wr;
+
+logic [1:0] [12:0] victim_tags_after_rd;
+logic [1:0] [63:0] victim_data_after_rd;
+logic [1:0] victim_valid_after_rd;
+logic [1:0] victim_dirty_after_rd;
+logic victim_lru_after_rd;
+logic [31:0] [63:0] data_after_rd;
+logic [31:0] [7:0] tags_after_rd; 
+logic [31:0] valid_after_rd;
+logic [31:0] dirty_after_rd;
+
+logic [1:0] [12:0] victim_tags_after_mem;
+logic [1:0] [63:0] victim_data_after_mem;
+logic [1:0] victim_valid_after_mem;
+logic [1:0] victim_dirty_after_mem;
+logic victim_lru_after_mem;
+logic [31:0] [63:0] data_after_mem;
+logic [31:0] [7:0] tags_after_mem; 
+logic [31:0] valid_after_mem;
+logic [31:0] dirty_after_mem;
+
+logic swap_dirty_tmp_wr;
+logic [63:0] swap_data_tmp_wr;
+logic [7:0] swap_tag_tmp_wr;
+logic swap_dirty_tmp_rd;
+logic [63:0] swap_data_tmp_rd;
+logic [7:0] swap_tag_tmp_rd;
+
+wire rd_cache_hit;
+wire [1:0] rd_victim_match;
+wire rd_victim_hit;
+wire wr_cache_hit;
+wire [1:0] wr_victim_match;
+wire wr_victim_hit;
+wor mem_cache_conflict;
+
+always_comb begin
+    case (proc_wr_size)
+        `BYTE:      proc_wr_mask = {56'b0, {8{1'b1}}};
+        `HALF:      proc_wr_mask = {48'b0, {16{1'b1}}};
+        `WORD:      proc_wr_mask = {32'b0, {32{1'b1}}};
+        default:    proc_wr_mask = {64{1'b1}};
+    endcase
+    proc_wr_mask = proc_wr_mask << {proc_wr_offset, 3'b0};
+
+    case (rd_size)
+        `BYTE:      proc_rd_mask = {56'b0, {8{1'b1}}};
+        `HALF:      proc_rd_mask = {48'b0, {16{1'b1}}};
+        `WORD:      proc_rd_mask = {32'b0, {32{1'b1}}};
+        default:    proc_rd_mask = {64{1'b1}};
+    endcase
+end
+
+// check if incoming mem already exists in cache/victim
+assign mem_cache_conflict = valid_after_rd[mem_wr_idx] && mem_wr_tag == tags_after_rd[mem_wr_idx];
+assign mem_cache_conflict = victim_valid_after_rd[0] && mem_wr_tag == victim_tags_after_rd[0][12:5];
+assign mem_cache_conflict = victim_valid_after_rd[1] && mem_wr_tag == victim_tags_after_rd[1][12:5];
+
+// check if read hit
+assign rd_data_cache_raw = data_after_wr[rd_idx];
+assign rd_data_cache = (rd_data_cache_raw >> {rd_offset, 3'b0}) & proc_rd_mask;
+assign rd_cache_hit = tags_after_wr[rd_idx] == rd_tag && valid_after_wr[rd_idx];
+assign rd_victim_hit = rd_victim_match != 0;
+genvar gi;
+for (gi = 0; gi < 2; ++gi) begin
+    assign rd_victim_match[gi] = victim_tags_after_wr[gi] == {rd_tag, rd_idx} && victim_valid_after_wr[gi];
+end
+
+// check if write hit
+assign wr_cache_hit = tags[rd_idx] == rd_tag && valid[rd_idx];
+assign wr_victim_hit = wr_victim_match != 0;
+for (gi = 0; gi < 2; ++gi) begin
+    assign wr_victim_match[gi] = victim_tags[gi] == {rd_tag, rd_idx} && victim_valid[gi];
+end
+
+// handle write
+always_comb begin
+    // internal states
+    victim_tags_after_wr = victim_tags;
+    victim_data_after_wr = victim_data;
+    victim_valid_after_wr = victim_valid;
+    victim_dirty_after_wr = victim_dirty;
+    victim_lru_after_wr = victim_lru;
+    data_after_wr = data;
+    tags_after_wr = tags; 
+    valid_after_wr = valid;
+    dirty_after_wr = dirty;
+    // outputs
+    wr_en_out = 0;
+    wr_addr_out = 0;
+    wr_data_out = 0;
+    wr_size_out = 0;
+    swap_data_tmp_wr = 0;
+    swap_dirty_tmp_wr = 0;
+    if (proc_wr_en) begin
+        if (wr_cache_hit) begin
+            // cache match: write directly to cache entry
+            data_after_wr[proc_wr_idx] = data_after_wr[proc_wr_idx] & (~proc_wr_mask);
+            data_after_wr[proc_wr_idx] = data_after_wr[proc_wr_idx] | (proc_wr_mask & (proc_wr_data << {proc_wr_offset, 3'b0}));
+            dirty_after_wr[proc_wr_idx] = 1'b1;
+        end
+        else if (wr_victim_hit) begin
+            // cache miss victim match: swap
+            victim_lru_after_wr = ~wr_victim_match[0];
+            for (int i = 0; i < 2; ++i) begin
+                if (wr_victim_match[i]) begin
+                    victim_data_after_wr[i] &= (~proc_wr_mask);
+                    victim_data_after_wr[i] |= (proc_wr_mask & (proc_wr_data << {proc_wr_offset, 3'b0}));
+                    victim_dirty_after_wr[i] = 1'b1;
+                    victim_tags_after_wr[i] = {tags[proc_wr_idx], proc_wr_idx};
+                    swap_data_tmp_wr = victim_data[i];
+                    swap_dirty_tmp_wr = victim_dirty[i];
+                    swap_tag_tmp_wr = victim_tags[i][12:5];
+                end
+            end
+            data_after_wr[proc_wr_idx] = swap_data_tmp_wr;
+            dirty_after_wr[proc_wr_idx] = swap_dirty_tmp_wr;
+            tags_after_wr[proc_wr_idx] = swap_tag_tmp_wr;
+        end
+        else begin
+            // cache miss victim miss: goto mem
+            wr_en_out = 1'b1;
+            wr_addr_out = {proc_wr_tag, proc_wr_idx, proc_wr_offset};
+            wr_data_out = proc_wr_data;
+            wr_size_out = proc_wr_size;
+        end
+    end
+end
+
+
+// handle read
+always_comb begin
+    // internal states
+    victim_tags_after_rd = victim_tags_after_wr;
+    victim_data_after_rd = victim_data_after_wr;
+    victim_valid_after_rd = victim_valid_after_wr;
+    victim_dirty_after_rd = victim_dirty_after_wr;
+    victim_lru_after_rd = victim_lru_after_wr;
+    data_after_rd = data_after_wr;
+    tags_after_rd = tags_after_wr;
+    valid_after_rd = valid_after_wr;
+    dirty_after_rd = dirty_after_wr;
+    // outputs
+    rd_data = 0;
+    rd_valid = 0;
+    rd_en_out = 0;
+    rd_addr_out = 0;
+    rd_gnt_out = 0;
+    if (rd_en) begin
+        if (rd_cache_hit) begin
+            // cache match: read directly from cache
+            rd_data = rd_data_cache;
+            rd_valid = 1'b1;
+        end
+        else if (rd_victim_hit) begin
+            // cache miss victim match: swap
+            victim_lru_after_rd = ~rd_victim_match[0];
+            for (int i = 0; i < 2; ++i) begin
+                if (rd_victim_match[i]) begin
+                    victim_data_after_rd[i] = data_after_wr[rd_idx];
+                    victim_dirty_after_rd[i] = dirty_after_wr[rd_idx];
+                    victim_tags_after_rd[i] = {tags_after_wr[rd_idx], rd_idx};
+                    swap_data_tmp_rd = victim_data_after_wr[i];
+                    swap_dirty_tmp_rd = victim_dirty_after_wr[i];
+                    swap_tag_tmp_rd = victim_tags_after_wr[i][12:5];
+                    rd_data = (victim_data_after_wr[i] >> {rd_offset, 3'b0}) & proc_rd_mask;
+                    rd_valid = 1'b1;
+                end
+            end
+            data_after_rd[rd_idx] = swap_data_tmp_rd;
+            dirty_after_rd[rd_idx] = swap_dirty_tmp_rd;
+            tags_after_rd[rd_idx] = swap_tag_tmp_rd;
+        end
+        else begin
+            // cache miss victim miss: goto mem
+            rd_en_out = 1'b1;
+            rd_addr_out = {rd_tag, rd_idx, 3'b0};
+            rd_gnt_out = rd_gnt;
+            rd_size_out = rd_size;
+        end
+    end
+end
+
+
+// handle incoming memory
+always_comb begin
+    // internal states
+    victim_tags_after_mem = victim_tags_after_rd;
+    victim_data_after_mem = victim_data_after_rd;
+    victim_valid_after_mem = victim_valid_after_rd;
+    victim_dirty_after_mem = victim_dirty_after_rd;
+    victim_lru_after_mem = victim_lru_after_rd;
+    data_after_mem = data_after_rd;
+    tags_after_mem = tags_after_rd;
+    valid_after_mem = valid_after_rd;
+    dirty_after_mem = dirty_after_rd;
+    // outputs
+    wb_en_out = 0;
+    wb_addr_out = 0;
+    wb_data_out = 0;
+    wb_size_out = 0;
+    if (mem_wr_en) begin
+        if (mem_cache_conflict) begin
+            // entry already exists in cache/victim, do nothing
+        end
+        else if (~valid_after_rd[mem_wr_idx]) begin
+            // overwrite directly
+            data_after_mem[mem_wr_idx] = mem_wr_data;
+            tags_after_mem[mem_wr_idx] = mem_wr_tag;
+            valid_after_mem[mem_wr_idx] = 1'b1;
+            dirty_after_mem[mem_wr_idx] = 1'b0;
+        end
+        else if (victim_valid_after_rd != 2'b11) begin
+            // evict cache entry, overwrite victim cache lru
+            // overwrite cache
+            data_after_mem[mem_wr_idx] = mem_wr_data;
+            tags_after_mem[mem_wr_idx] = mem_wr_tag;
+            valid_after_mem[mem_wr_idx] = 1'b1;
+            dirty_after_mem[mem_wr_idx] = 1'b0;
+            // overwrite victim lru
+            victim_tags_after_mem = tags_after_rd[mem_wr_idx];
+            victim_data_after_mem = data_after_rd[mem_wr_idx];
+            victim_valid_after_mem = valid_after_rd[mem_wr_idx];
+            victim_dirty_after_mem = dirty_after_rd[mem_wr_idx];
+            victim_lru_after_mem = ~victim_lru_after_rd;
+        end
+        else begin
+            // evict cache entry, evict victim cache lru, write back to mem
+            // overwrite cache
+            data_after_mem[mem_wr_idx] = mem_wr_data;
+            tags_after_mem[mem_wr_idx] = mem_wr_tag;
+            valid_after_mem[mem_wr_idx] = 1'b1;
+            dirty_after_mem[mem_wr_idx] = 1'b0;
+            // overwrite victim lru
+            victim_tags_after_mem = tags_after_rd[mem_wr_idx];
+            victim_data_after_mem = data_after_rd[mem_wr_idx];
+            victim_valid_after_mem = valid_after_rd[mem_wr_idx];
+            victim_dirty_after_mem = dirty_after_rd[mem_wr_idx];
+            victim_lru_after_mem = ~victim_lru_after_rd;
+            // write back
+            if (victim_dirty_after_rd[victim_lru_after_rd]) begin
+                wb_en_out = 1'b1;
+                wb_addr_out = {victim_tags_after_rd[victim_lru_after_rd], 3'b0};
+                wb_data_out = victim_data_after_rd[victim_lru_after_rd];
+                wb_size_out = `DOUBLE;
+            end
+        end
+    end
+end
+
+always_ff @(posedge clock) begin
+    if (reset) begin
+        data = 0;
+        tags = 0; 
+        valid = 0;
+        dirty = 0;
+        victim_tags = 0;
+        victim_data = 0;
+        victim_valid = 0;
+        victim_dirty = 0;
+        victim_lru = 0;
+    end
+    else begin
+        data = data_after_mem;
+        tags = tags_after_mem; 
+        valid = valid_after_mem;
+        dirty = dirty_after_mem;
+        victim_tags = victim_tags_after_mem;
+        victim_data = victim_data_after_mem;
+        victim_valid = victim_valid_after_mem;
+        victim_dirty = victim_dirty_after_mem;
+        victim_lru = victim_lru_after_mem;
+    end
+end
+
+endmodule
---
+++ verilog/dcache_ctrl.sv
@@ -0,0 +1,378 @@
+// write-back no write allocate dcache controller
+// serialize only, no forwarding
+
+`define LSQSZ 16
+`define BYTE 2'b0
+`define HALF 2'h1
+`define WORD 2'h2
+`define DOUBLE 2'h3
+`define MEM_SIZE [1:0]
+
+module dcache_ctrl(
+    input                       clock,
+    input                       reset,
+    input                       except,   // cancel all reads at mispredict? 
+
+    // write dirty entries back
+    input                       wb_en_in,
+    input [15:0]                wb_addr_in,
+    input [63:0]                wb_data_in,
+    input `MEM_SIZE             wb_size_in,
+
+    // write directly to mem on wr miss
+    input                       wr_en_in,
+    input [15:0]                wr_addr_in,
+    input [63:0]                wr_data_in,
+    input `MEM_SIZE             wr_size_in,
+
+    // read from mem on rd miss
+    input                       rd_en_in,
+    input [15:0]                rd_addr_in,
+    input [`LSQSZ-1:0]          rd_gnt_in,
+    input `MEM_SIZE             rd_size_in, // used for lsq feedback only, always ask for DOUBLE from mem
+
+    // from mem
+    input [3:0]                 mem2proc_response,// 0 = can't accept, other=tag of transaction
+	input [63:0]                mem2proc_data,    // data resulting from a load
+	input [3:0]                 mem2proc_tag,     // 0 = no value, other=tag of transaction
+
+    // to mem
+    output wor [1:0]            Dmem_command, 
+    output logic [15:0]         Dmem_addr,
+    output wor `MEM_SIZE        Dmem_size,
+
+    // feedback to lsq
+    output logic [`LSQSZ-1:0]   mem_feedback,
+    output logic [31:0]         mem_data,
+
+    // to dcache
+    output logic                mem_wr_en,
+    output logic [4:0]          mem_wr_idx,
+    output logic [7:0]          mem_wr_tag,
+    output logic [63:0]         mem_wr_data
+);
+
+parameter WIDTH = 2 * `LSQSZ;
+
+reg [2:0] valid_new_reg;
+reg [2:0] [15:0] addr_new_reg;
+reg [2:0] is_wr_new_reg;
+reg [2:0] is_rd_new_reg;
+reg [2:0] `MEM_SIZE sz_new_reg;
+reg [2:0] [`LSQSZ-1:0] rd_gnt_new_reg;
+
+// q1 entries has not gone to mem
+reg [WIDTH-1:0] [15:0] addr_q1_reg;
+reg [WIDTH-1:0] is_wr_q1_reg;
+reg [WIDTH-1:0] is_rd_q1_reg;
+reg [WIDTH-1:0] `MEM_SIZE sz_q1_reg;
+reg [WIDTH-1:0] [`LSQSZ-1:0] rd_gnt_q1_reg;
+
+// q2 entries waiting for mem response one by one, contains both reads and writes
+reg [`LSQSZ-1:0] [15:0] addr_q2_reg;
+reg [`LSQSZ-1:0] is_wr_q2_reg;
+reg [`LSQSZ-1:0] is_rd_q2_reg;
+reg [`LSQSZ-1:0] [`LSQSZ-1:0] rd_gnt_q2_reg;
+reg [`LSQSZ-1:0] [3:0] mem_tag_q2_reg;
+reg [`LSQSZ-1:0] `MEM_SIZE sz_q2_reg;
+
+reg [15:0] q1_head_addr_reg;
+reg q1_head_is_wr_reg;
+reg q1_head_is_rd_reg;
+reg `MEM_SIZE q1_head_sz_reg;
+reg [`LSQSZ-1:0] q1_head_rd_gnt_reg;
+
+reg [15:0] q2_head_addr_reg;
+reg q2_head_is_wr_reg;
+reg q2_head_is_rd_reg;
+reg [`LSQSZ-1:0] q2_head_rd_gnt_reg;
+reg [3:0] q2_head_mem_tag_reg;
+reg `MEM_SIZE q2_head_sz_reg;
+
+reg [WIDTH-1:0] q1_head_reg;
+reg [WIDTH-1:0] q1_tail_reg;
+reg [`LSQSZ-1:0] q2_head_reg;
+reg [`LSQSZ-1:0] q2_tail_reg;
+
+
+wire q2_head_mem_response;
+assign q2_head_mem_response = q2_head_mem_tag_reg == mem2proc_tag & q2_head_is_rd_reg;
+
+genvar gi, gj;
+
+// add new entries, clear old entries
+// q2
+wor [`LSQSZ-1:0] [15:0] addr_q2_next;
+wor [`LSQSZ-1:0] is_wr_q2_next;
+wor [`LSQSZ-1:0] is_rd_q2_next;
+wor [`LSQSZ-1:0] [`LSQSZ-1:0] rd_gnt_q2_next;
+wor [`LSQSZ-1:0] [3:0] mem_tag_q2_next;
+wor [`LSQSZ-1:0] `MEM_SIZE sz_q2_next;
+
+wor [15:0] q2_head_addr_next;
+wor q2_head_is_wr_next;
+wor q2_head_is_rd_next;
+wor [`LSQSZ-1:0] q2_head_rd_gnt_next;
+wor [3:0] q2_head_mem_tag_next;
+wor `MEM_SIZE q2_head_sz_next;
+
+wire [`LSQSZ-1:0] q2_head_tmp;
+wire [`LSQSZ-1:0] q2_head_next;
+wire q2_incoming; 
+wire q2_empty;
+wire [`LSQSZ-1:0] q2_tail_next;
+
+assign q2_empty = q2_head_reg == q2_tail_reg;
+assign q2_incoming = q1_head_is_wr_reg | q1_head_is_rd_reg;
+assign q2_head_tmp = q2_head_mem_response ? {q2_head_reg[`LSQSZ-2:0], q2_head_reg[`LSQSZ-1]} : q2_head_reg;
+assign q2_head_next = q2_empty ? q2_head_reg : q2_head_tmp;
+assign q2_tail_next = q2_incoming ? {q2_tail_reg[`LSQSZ-2:0], q2_tail_reg[`LSQSZ-1]} : q2_tail_reg;
+generate;
+    // q2 head reg
+    for (gi = 0; gi < `LSQSZ; ++gi) begin
+        // note: no fwd to q2
+        assign q2_head_addr_next = q2_head_next[gi] ? addr_q2_next[gi] : 0;
+        assign q2_head_is_wr_next = q2_head_next[gi] ? is_wr_q2_next[gi] : 0;
+        assign q2_head_is_rd_next = q2_head_next[gi] ? is_rd_q2_next[gi] : 0;
+        assign q2_head_rd_gnt_next = q2_head_next[gi] ? rd_gnt_q2_next[gi] : 0;
+        assign q2_head_mem_tag_next = q2_head_next[gi] ? mem_tag_q2_next[gi] : 0;
+        assign q2_head_sz_next = q2_head_next[gi] ? sz_q2_next[gi] : 0;
+    end
+    // clear curr q2 head
+    for (gi = 0; gi < `LSQSZ; ++gi) begin
+        assign addr_q2_next[gi] = (q2_head_reg[gi] & q2_head_mem_response) ? 0 : addr_q2_reg[gi];
+        assign is_wr_q2_next[gi] = (q2_head_reg[gi] & q2_head_mem_response) ? 0 : is_wr_q2_reg[gi];
+        assign is_rd_q2_next[gi] = (q2_head_reg[gi] & q2_head_mem_response) ? 0 : (is_rd_q2_reg[gi] & ~except);
+        assign rd_gnt_q2_next[gi] = (q2_head_reg[gi] & q2_head_mem_response) ? 0 : rd_gnt_q2_reg[gi];
+        assign mem_tag_q2_next[gi] = (q2_head_reg[gi] & q2_head_mem_response) ? 0 : mem_tag_q2_reg[gi];
+        assign sz_q2_next[gi] = (q2_head_reg[gi] & q2_head_mem_response) ? 0 : sz_q2_reg[gi];
+    end
+    // write curr q2 tail
+    for (gi = 0; gi < `LSQSZ; ++gi) begin
+        assign addr_q2_next[gi] = (q2_incoming & q2_tail_reg[gi]) ? q1_head_addr_reg : 0;
+        assign is_wr_q2_next[gi] = (q2_incoming & q2_tail_reg[gi]) ? q1_head_is_wr_reg : 0;
+        assign is_rd_q2_next[gi] = (q2_incoming & q2_tail_reg[gi]) ? (q1_head_is_rd_reg & ~except) : 0;
+        assign rd_gnt_q2_next[gi] = (q2_incoming & q2_tail_reg[gi]) ? q1_head_rd_gnt_reg : 0;
+        assign mem_tag_q2_next[gi] = (q2_incoming & q2_tail_reg[gi]) ? mem2proc_response : 0;
+        assign sz_q2_next[gi] = (q2_incoming & q2_tail_reg[gi]) ? q1_head_sz_reg : 0;
+    end
+endgenerate
+
+always_ff @(posedge clock) begin
+    if (reset) begin
+        addr_q2_reg <= 0;
+        is_wr_q2_reg <= 0;
+        is_rd_q2_reg <= 0;
+        rd_gnt_q2_reg <= 0;
+        mem_tag_q2_reg <= 0;
+        sz_q2_reg <= 0;
+
+        q2_head_addr_reg <= 0;
+        q2_head_is_wr_reg <= 0;
+        q2_head_is_rd_reg <= 0;
+        q2_head_rd_gnt_reg <= 0;
+        q2_head_mem_tag_reg <= 0;
+        q2_head_sz_reg <= 0;
+
+        q2_head_reg <= `LSQSZ'b1;
+        q2_tail_reg <= `LSQSZ'b1;
+    end
+    else begin
+        addr_q2_reg <= addr_q2_next;
+        is_wr_q2_reg <= is_wr_q2_next;
+        is_rd_q2_reg <= is_rd_q2_next;
+        rd_gnt_q2_reg <= rd_gnt_q2_next;
+        mem_tag_q2_reg <= mem_tag_q2_next;
+        sz_q2_reg <= sz_q2_next;
+
+        q2_head_addr_reg <= q2_head_addr_next;
+        q2_head_is_wr_reg <= q2_head_is_wr_next;
+        q2_head_is_rd_reg <= q2_head_is_rd_next;
+        q2_head_rd_gnt_reg <= q2_head_rd_gnt_next;
+        q2_head_mem_tag_reg <= q2_head_mem_tag_next;
+        q2_head_sz_reg <= q2_head_sz_next;
+
+        q2_head_reg <= q2_head_next;
+        q2_tail_reg <= q2_tail_next;
+    end
+end
+
+// new entries
+wire [2:0] [2:0] insert_pos;
+assign insert_pos[0] = {2'b0, wb_en_in | wr_en_in | rd_en_in};
+assign insert_pos[1] = wb_en_in ? {1'b0, wr_en_in | rd_en_in, 1'b0} : {2'b0, wr_en_in | rd_en_in};
+assign insert_pos[2] = {wb_en_in & wr_en_in & rd_en_in, 2'b0};
+
+wor [2:0] valid_new_in;
+wor [2:0] [15:0] addr_new_in;
+wor [2:0] is_wr_new_in;
+wor [2:0] is_rd_new_in;
+wor [2:0] `MEM_SIZE sz_new_in;
+wor [2:0] [`LSQSZ-1:0] rd_gnt_new_in;
+assign valid_new_in = {rd_en_in, wr_en_in, wb_en_in};
+assign addr_new_in = {rd_addr_in, wr_addr_in, wb_addr_in};
+assign is_wr_new_in = {1'b0, wr_en_in, wb_en_in};
+assign is_rd_new_in = {rd_addr_in, 2'b0};
+assign sz_new_in = {rd_size_in, wr_size_in, `DOUBLE};
+assign rd_gnt_new_in = {rd_gnt_in, `LSQSZ'b0, `LSQSZ'b0};
+
+wor [2:0] valid_new_next;
+wor [2:0] [15:0] addr_new_next;
+wor [2:0] is_wr_new_next;
+wor [2:0] is_rd_new_next;
+wor [2:0] `MEM_SIZE sz_new_next;
+wor [2:0] [`LSQSZ-1:0] rd_gnt_new_next;
+
+for (gi = 0; gi < 3; ++gi) begin
+    for (gj = 0; gj < 3; ++gj) begin
+        assign valid_new_next[gj] = insert_pos[gi][gj];
+        assign addr_new_next[gj] = insert_pos[gi][gj] ? addr_new_in[gi] : 0;
+        assign is_wr_new_next[gj] = insert_pos[gi][gj] ? is_wr_new_in[gi] : 0;
+        assign is_rd_new_next[gj] = insert_pos[gi][gj] ? (is_rd_new_in[gi] & ~except) : 0;
+        assign sz_new_next[gj] = insert_pos[gi][gj] ? sz_new_in[gi] : 0;
+        assign rd_gnt_new_next[gj] = insert_pos[gi][gj] ? rd_gnt_new_next[gi] : 0;
+    end
+end
+
+always_ff @(posedge clock) begin
+    if (reset) begin
+        valid_new_reg <= 0;
+        addr_new_reg <= 0;
+        is_wr_new_reg <= 0;
+        is_rd_new_reg <= 0;
+        sz_new_reg <= 0;
+        rd_gnt_new_reg <= 0;
+    end
+    else begin
+        valid_new_reg <= valid_new_next;
+        addr_new_reg <= addr_new_next;
+        is_wr_new_reg <= is_wr_new_next;
+        is_rd_new_reg <= is_rd_new_next;
+        sz_new_reg <= sz_new_next;
+        rd_gnt_new_reg <= rd_gnt_new_next;
+    end
+end
+
+// q1
+wor [WIDTH-1:0] [15:0] addr_q1_next;
+wor [WIDTH-1:0] is_wr_q1_next;
+wor [WIDTH-1:0] is_rd_q1_next;
+wor [WIDTH-1:0] `MEM_SIZE sz_q1_next;
+wor [WIDTH-1:0] [`LSQSZ-1:0] rd_gnt_q1_next;
+
+wor [15:0] q1_head_addr_next;
+wor q1_head_is_wr_next;
+wor q1_head_is_rd_next;
+wor `MEM_SIZE q1_head_sz_next;
+wor [`LSQSZ-1:0] q1_head_rd_gnt_next;
+
+wire [WIDTH-1:0] q1_head_next;
+wire [WIDTH-1:0] q1_tail_next;
+wire [3:0] [WIDTH-1:0] q1_tail_tmp;
+wire q1_empty;
+assign q1_empty = q1_head_reg == q1_tail_reg;
+assign q1_head_next = q1_empty ? q1_head_reg : {q1_head_reg[WIDTH-2:0], q1_head_reg[WIDTH-1]};
+
+assign q1_tail_tmp[0] = q1_tail_reg;
+generate;
+    // positions to insert
+    for (gi = 1; gi < 4; ++gi) begin
+        assign q1_tail_tmp[gi] = valid_new_reg[gi-1] ? {q1_tail_tmp[gi-1][WIDTH-2:0], q1_tail_tmp[gi-1][WIDTH-1]} 
+            : q1_tail_tmp[gi-1];
+    end
+    assign q1_tail_next = q1_tail_tmp[3];
+    // q1 head reg
+    for (gi = 0; gi < WIDTH; ++gi) begin
+        // note: no fwd to q1 head
+        assign q1_head_addr_next = q1_head_next[gi] ? addr_q1_next[gi] : 0;
+        assign q1_head_is_wr_next = q1_head_next[gi] ? is_wr_q1_next[gi] : 0;
+        assign q1_head_is_rd_next = q1_head_next[gi] ? is_rd_q1_next[gi] : 0;
+        assign q1_head_rd_gnt_next = q1_head_next[gi] ? rd_gnt_q1_next[gi] : 0;
+        assign q1_head_sz_next = q1_head_next[gi] ? sz_q1_next[gi] : 0;
+    end
+    // clear curr q1 head
+    for (gi = 0; gi < WIDTH; ++gi) begin
+        assign addr_q1_next[gi] = q1_head_reg[gi] ? 0 : addr_q1_reg[gi];
+        assign is_wr_q1_next[gi] = q1_head_reg[gi] ? 0 : is_wr_q1_reg[gi];
+        assign is_rd_q1_next[gi] = q1_head_reg[gi] ? 0 : (is_rd_q1_reg[gi] & ~except);
+        assign rd_gnt_q1_next[gi] = q1_head_reg[gi] ? 0 : rd_gnt_q1_reg[gi];
+        assign sz_q1_next[gi] = q1_head_reg[gi] ? 0 : sz_q1_reg[gi];
+    end
+    // add new entries to tail
+    for (gi = 0; gi < 3; ++gi) begin
+        for (gj = 0; gj < WIDTH; ++gj) begin
+            assign addr_q1_next[gj] = q1_tail_tmp[gi][gj] ? addr_new_reg[gi] : 0;
+            assign is_wr_q1_next[gj] = q1_tail_tmp[gi][gj] ? is_wr_new_reg[gi] : 0;
+            assign is_rd_q1_next[gj] = q1_tail_tmp[gi][gj] ? (is_rd_new_reg[gi] & ~except) : 0;
+            assign rd_gnt_q1_next[gj] = q1_tail_tmp[gi][gj] ? rd_gnt_new_reg[gi] : 0;
+            assign sz_q1_next[gj] = q1_tail_tmp[gi][gj] ? sz_new_reg[gi] : 0;
+        end
+    end
+endgenerate
+
+always_ff @(posedge clock) begin
+    if (reset) begin
+        addr_q1_reg <= 0;
+        is_wr_q1_reg <= 0;
+        is_rd_q1_reg <= 0;
+        sz_q1_reg <= 0;
+        rd_gnt_q1_reg <= 0;
+
+        q1_head_addr_reg <= 0;
+        q1_head_is_wr_reg <= 0;
+        q1_head_is_rd_reg <= 0;
+        q1_head_sz_reg <= 0;
+        q1_head_rd_gnt_reg <= 0;
+
+        q1_head_reg <= {{(WIDTH-1){1'b0}}, 1'b1};
+        q1_tail_reg <= {{(WIDTH-1){1'b0}}, 1'b1};
+    end
+    else begin
+        addr_q1_reg <= addr_q1_next;
+        is_wr_q1_reg <= is_wr_q1_next;
+        is_rd_q1_reg <= is_rd_q1_next;
+        sz_q1_reg <= sz_q1_next;
+        rd_gnt_q1_reg <= rd_gnt_q1_next;
+
+        q1_head_addr_reg <= q1_head_addr_next;
+        q1_head_is_wr_reg <= q1_head_is_wr_next;
+        q1_head_is_rd_reg <= q1_head_is_rd_next;
+        q1_head_sz_reg <= q1_head_sz_next;
+        q1_head_rd_gnt_reg <= q1_head_rd_gnt_next;
+
+        q1_head_reg <= q1_head_next;
+        q1_tail_reg <= q1_tail_next;
+    end
+end
+
+// q1 head to mem
+assign Dmem_command = q1_head_is_rd_reg ? BUS_LOAD : BUS_NONE;
+assign Dmem_command = q1_head_is_wr_reg ? BUS_STORE : BUS_NONE;
+assign Dmem_addr = q1_head_addr_reg;
+assign Dmem_size = q1_head_is_rd_reg ? `DOUBLE : 0;
+assign Dmem_size = q1_head_is_wr_reg ? q1_head_sz_reg : 0;
+
+// feedback to lsq & dcache, responds only if is_rd
+assign mem_wr_en = q2_head_mem_response;
+assign mem_wr_idx = q2_head_addr_reg[7:3];
+assign mem_wr_tag = q2_head_addr_reg[15:8];
+assign mem_wr_data = mem2proc_data;
+
+logic [63:0] mem_data_msk;
+wire [63:0] mem_data_tmp;
+
+assign mem_feedback = q2_head_mem_response ? q2_head_rd_gnt_reg : 0;
+always_comb begin
+    case(q2_head_sz_reg)
+        `BYTE:      mem_data_msk = {56'b0, {8{1'b1}}};
+        `HALF:      mem_data_msk = {48'b0, {16{1'b1}}};
+        `WORD:      mem_data_msk = {32'b0, {32{1'b1}}};
+        default:    mem_data_msk = {64{1'b1}};
+    endcase
+end
+assign mem_data_tmp = (mem2proc_data >> {q2_head_addr_reg[2:0], 3'b0}) & mem_data_msk;
+assign mem_data = q2_head_mem_response ? mem_data_tmp[31:0] : 0;
+
+
+endmodule
+
